3a199a63d03adccfe175d6442411f74d63e699a7
savageme6ix <maingithomas803@gmail.com>
2026-03-04 19:09:24 +0300

    initated json

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,32 @@ import { AddItem } from './AddItem';
 import SearchItem from './SearchItem';
 function App() {
 
-   const [items,setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
+    const API_URL ='http://localhost:3500/items';
+
+   const [items,setItems] = useState([]);
 
     const [newItem, setNewItem] = useState('');
     const [search, setSearch] = useState('');
 
-    const setAndSave = (newItems) =>{
-        setItems(newItems)
-        localStorage.setItem('shoppingList', JSON.stringify(newItems));
-    }
-
+    useEffect(()=>{
+        
+    },[])
+    
     const addItem = (item)=>{
         const id = items.length ? items[items.length-1].id + 1 : 1;
         const myNewItem = {id,checked:false, item}
         const listItems = [...items, myNewItem]
-        setAndSave(listItems)
+        setItems(listItems)
     }
 
     function handleCheck(id){
         const listItems = items.map((item)=> item.id === id ? {...item, checked: !item.checked} : item);
-        setAndSave(listItems)
+        setItems(listItems)
     }
 
     function handleDelete(id){
         const listItems = items.filter((item)=> item.id !== id);
-        setAndSave(listItems)
+        setItems(listItems)
     }
 
     function handleSubmit(e){
